e74e61311ca56c0a727f2e8a68811139677f3b79
Ege Güneş <ege.gunes@percona.com>
2023-07-16 13:29:20 +0300

    fix tests

--- stream_framework/settings.py
+++ stream_framework/settings.py
@@ -1,14 +1,14 @@
 from stream_framework.default_settings import *
 
-'''
+"""
 Please fork and add hooks to import your custom settings system.
 Right now we only support Django, but the intention is to support
 any settings system
-'''
+"""
 
 
-def import_global_module(module, current_locals, current_globals, exceptions=None):
-    '''Import the requested module into the global scope
+def import_global_module(module, current_locals, current_globals, exceptions=()):
+    """Import the requested module into the global scope
     Warning! This will import your module into the global scope
 
     **Example**:
@@ -20,13 +20,13 @@ def import_global_module(module, current_locals, current_globals, exceptions=Non
     :param current_globals: the current globals
     :param exceptions: the exceptions which to ignore while importing
 
-    '''
+    """
     try:
         try:
-            objects = getattr(module, '__all__', dir(module))
+            objects = getattr(module, "__all__", dir(module))
 
             for k in objects:
-                if k and k[0] != '_':
+                if k and k[0] != "_":
                     current_globals[k] = getattr(module, k)
         except exceptions as e:
             return e
@@ -36,10 +36,12 @@ def import_global_module(module, current_locals, current_globals, exceptions=Non
 
 try:
     import django
-    settings_system = 'django'
+
+    settings_system = "django"
 except ImportError as e:
     settings_system = None
 
-if settings_system == 'django':
+if settings_system == "django":
     from django.conf import settings
+
     import_global_module(settings, locals(), globals())
--- stream_framework/tests/managers/base.py
+++ stream_framework/tests/managers/base.py
@@ -1,19 +1,21 @@
-import datetime
-from stream_framework.feed_managers.base import Manager
-from stream_framework.tests.utils import Pin
-from stream_framework.tests.utils import FakeActivity
-from stream_framework.verbs.base import Love as LoveVerb
-from mock import patch
-import unittest
 import copy
+import datetime
+import unittest
 from functools import partial
+
+from mock import patch
+
+from stream_framework.feed_managers.base import Manager
+from stream_framework.tests.utils import FakeActivity, Pin
+from stream_framework.verbs.base import Love as LoveVerb
 
 
 def implementation(meth):
     def wrapped_test(self, *args, **kwargs):
         if self.__class__ == BaseManagerTest:
-            raise unittest.SkipTest('only test this on actual implementations')
+            raise unittest.SkipTest("only test this on actual implementations")
         return meth(self, *args, **kwargs)
+
     return wrapped_test
 
 
@@ -24,9 +26,11 @@ class BaseManagerTest(unittest.TestCase):
         self.manager = self.manager_class()
         self.actor_id = 42
         self.pin = Pin(
-            id=1, created_at=datetime.datetime.now() - datetime.timedelta(hours=1))
+            id=1, created_at=datetime.datetime.now() - datetime.timedelta(hours=1)
+        )
         self.activity = FakeActivity(
-            self.actor_id, LoveVerb, self.pin, 1, datetime.datetime.now(), {})
+            self.actor_id, LoveVerb, self.pin, 1, datetime.datetime.now(), {}
+        )
 
         if self.__class__ != BaseManagerTest:
             for user_id in list(range(1, 4)) + [17, 42, 44]:
@@ -36,10 +40,13 @@ class BaseManagerTest(unittest.TestCase):
 
     @implementation
     def test_add_user_activity(self):
-        assert self.manager.get_user_feed(
-            self.actor_id).count() == 0, 'the test feed is not empty'
+        assert (
+            self.manager.get_user_feed(self.actor_id).count() == 0
+        ), "the test feed is not empty"
 
-        with patch.object(self.manager, 'get_user_follower_ids', return_value={None: [1]}) as get_user_follower_ids:
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value={None: [1]}
+        ) as get_user_follower_ids:
             self.manager.add_user_activity(self.actor_id, self.activity)
             get_user_follower_ids.assert_called_with(user_id=self.actor_id)
 
@@ -49,10 +56,13 @@ class BaseManagerTest(unittest.TestCase):
 
     @implementation
     def test_batch_import(self):
-        assert self.manager.get_user_feed(
-            self.actor_id).count() == 0, 'the test feed is not empty'
+        assert (
+            self.manager.get_user_feed(self.actor_id).count() == 0
+        ), "the test feed is not empty"
 
-        with patch.object(self.manager, 'get_user_follower_ids', return_value={None: [1]}) as get_user_follower_ids:
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value={None: [1]}
+        ) as get_user_follower_ids:
             activities = [self.activity]
             self.manager.batch_import(self.actor_id, activities, 10)
             get_user_follower_ids.assert_called_with(user_id=self.actor_id)
@@ -71,23 +81,29 @@ class BaseManagerTest(unittest.TestCase):
         # error
         activity = copy.deepcopy(self.activity)
         activity.actor_id = 10
-        with patch.object(self.manager, 'get_user_follower_ids', return_value={None: [1]}):
-            batch = partial(
-                self.manager.batch_import, self.actor_id, [activity], 10)
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value={None: [1]}
+        ):
+            batch = partial(self.manager.batch_import, self.actor_id, [activity], 10)
             self.assertRaises(ValueError, batch)
 
     @implementation
     def test_add_remove_user_activity(self):
         user_id = 42
-        assert self.manager.get_user_feed(
-            user_id).count() == 0, 'the test feed is not empty'
+        assert (
+            self.manager.get_user_feed(user_id).count() == 0
+        ), "the test feed is not empty"
 
-        with patch.object(self.manager, 'get_user_follower_ids', return_value={None: [1]}) as get_user_follower_ids:
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value={None: [1]}
+        ) as get_user_follower_ids:
             self.manager.add_user_activity(user_id, self.activity)
             get_user_follower_ids.assert_called_with(user_id=user_id)
         assert self.manager.get_user_feed(user_id).count() == 1
 
-        with patch.object(self.manager, 'get_user_follower_ids', return_value={None: [1]}) as get_user_follower_ids:
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value={None: [1]}
+        ) as get_user_follower_ids:
             self.manager.remove_user_activity(user_id, self.activity)
             get_user_follower_ids.assert_called_with(user_id=user_id)
         assert self.manager.get_user_feed(user_id).count() == 0
@@ -96,13 +112,16 @@ class BaseManagerTest(unittest.TestCase):
     def test_add_user_activity_fanout(self):
         user_id = 42
         followers = {None: [1, 2, 3]}
-        assert self.manager.get_user_feed(
-            user_id).count() == 0, 'the test feed is not empty'
+        assert (
+            self.manager.get_user_feed(user_id).count() == 0
+        ), "the test feed is not empty"
 
         for follower in followers.values():
             assert self.manager.get_user_feed(follower).count() == 0
 
-        with patch.object(self.manager, 'get_user_follower_ids', return_value=followers) as get_user_follower_ids:
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value=followers
+        ) as get_user_follower_ids:
             self.manager.add_user_activity(user_id, self.activity)
             get_user_follower_ids.assert_called_with(user_id=user_id)
 
@@ -120,11 +139,15 @@ class BaseManagerTest(unittest.TestCase):
         follower_user_id = 42
 
         control_pin = Pin(
-            id=2, created_at=datetime.datetime.now() - datetime.timedelta(hours=1))
+            id=2, created_at=datetime.datetime.now() - datetime.timedelta(hours=1)
+        )
         control_activity = FakeActivity(
-            target_user_id, LoveVerb, control_pin, 2, datetime.datetime.now(), {})
+            target_user_id, LoveVerb, control_pin, 2, datetime.datetime.now(), {}
+        )
 
-        with patch.object(self.manager, 'get_user_follower_ids', return_value={}) as get_user_follower_ids:
+        with patch.object(
+            self.manager, "get_user_follower_ids", return_value={}
+        ) as get_user_follower_ids:
             self.manager.add_user_activity(target2_user_id, control_activity)
             self.manager.add_user_activity(target_user_id, self.activity)
             get_user_follower_ids.assert_called_with(user_id=target_user_id)
@@ -145,8 +168,7 @@ class BaseManagerTest(unittest.TestCase):
         for f in self.manager.get_feeds(follower_user_id).values():
             self.assertEqual(f.count(), 2)
 
-        self.manager.unfollow_user(
-            follower_user_id, target_user_id, async=False)
+        self.manager.unfollow_user(follower_user_id, target_user_id, async_=False)
 
         # make sure only one activity was removed
         for f in self.manager.get_feeds(follower_user_id).values():
